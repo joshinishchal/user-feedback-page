de424317e83ec5690bf4ec3c559cb644284f7141
netpulse <njoshi@netpulse.com>
2016-05-10 17:19:51 -0700

    ENTD-36, Started using angularFire and firebase.

--- compass-feedback/js/controllers/feedbackController.js
+++ compass-feedback/js/controllers/feedbackController.js
@@ -1,13 +1,26 @@
-var feedbackApp = angular.module("feedbackApp", []);
+var feedbackApp = angular.module("feedbackApp", ["ngResource", "firebase"]);
 
-feedbackApp.controller("feedbackController", function($scope){
+feedbackApp.constant("FirebaseUrl", "https://netpulse-feedback.firebaseio.com");
 
-	//$scope.url
+feedbackApp.constant("ChainName", "Three Rivers");
+
+feedbackApp.constant("ClubId" , "27b30fad-c756-4d96-afe0-6d3cb0ebc425");
+
+feedbackApp.constant("OverViewKey", "0000");
+
+feedbackApp.service("rootRef", ["FirebaseUrl", Firebase]);
+
+feedbackApp.service("club", function club(rootRef, $firebaseObject, ChainName, ClubId, OverViewKey) {
+	var overView = rootRef.child(ChainName).child(ClubId).child(OverViewKey);
+	this.overView = $firebaseObject(overView);
+})
+
+
+feedbackApp.controller("feedbackController", function($scope, club){
+
+	$scope.overView = club.overView;
+
 	$scope.clubInfo = {
 		clubName : "California Avenue"
 	};
-
-	//$scope.feedbackDetails = {};
-
-	//$resource.
 });
