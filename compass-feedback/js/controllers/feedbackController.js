134557aa483461db337cd7324f11fb989748cbe6
netpulse <njoshi@netpulse.com>
2016-05-11 17:57:43 -0700

    ENTD-36, added functionality to show fetch data from server, load 1 day data at a time and load more based on click on load more button

--- compass-feedback/js/controllers/feedbackController.js
+++ compass-feedback/js/controllers/feedbackController.js
@@ -17,17 +17,72 @@ feedbackApp.service("club", function club(rootRef, $firebaseObject, ChainName, C
 	this.getOverView = function() {
 		return $firebaseObject(overView);
 	};
-	this.getFeedbackData = function(){
-		return $firebaseObject(clubId);
+	this.getAllData = function(index){
+		return $firebaseObject(clubId.limitToLast(index));
 	};
 })
 
 
-feedbackApp.controller("feedbackController", function($scope, club){
+feedbackApp.controller("feedbackController", function($scope, club, OverViewKey){
+
+	$scope.OverViewKey = OverViewKey;
+	$scope.daysTobeViewed = 1;
 
 	$scope.overView = club.getOverView();
 
-	$scope.feedbackData = club.getFeedbackData();
+	$scope.allData = club.getAllData($scope.daysTobeViewed);
+
+	$scope.getMoreData = function(){
+		$scope.daysTobeViewed++;
+		$scope.allData = club.getAllData($scope.daysTobeViewed);
+	};
+
+	$scope.callText = "Call";
+	$scope.emailText = "Email";
+
+	$scope.getPhoneText = function(feedbackPhone, defaultPhoneText){
+		var phoneText = defaultPhoneText;
+		if(feedbackPhone && feedbackPhone != ""){
+			phoneText = feedbackPhone;
+		}
+		return phoneText;
+	};
+
+	$scope.getEmailText = function(feedbackEmail, defaultEmailText){
+		var emailText = defaultEmailText;
+		if(feedbackEmail && feedbackEmail != ""){
+			emailText = feedbackEmail;
+		}
+		return emailText;
+	};
+
+	$scope.getReasons = function(reason1,reason2,reason3,reason4,reason5,reason6){
+
+		var reasons = reason1;
+		reasons = addReasons(reasons,reason2);
+		reasons = addReasons(reasons,reason3);
+		reasons = addReasons(reasons,reason4);
+		reasons = addReasons(reasons,reason5);
+		reasons = addReasons(reasons,reason6);
+		return reasons;
+		
+	};
+
+	function addReasons(reasons,newReason){
+		var joiningString = "";
+
+		if(reasons != ""){
+			joiningString = ",";
+		}
+
+		if(newReason != ""){
+			return reasons + joiningString + newReason;
+		}else{
+			return reasons;
+		}
+	}
+
+
 
 	$scope.clubInfo = {
 		clubName : "California Avenue"
